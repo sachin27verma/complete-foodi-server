3db143f3c4dbeed4844f255a546ff26983785041
Anonymous <anonymous@example.com>
2024-07-16 13:23:40 +0530

    6001

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT||6001 ;
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
