29f48c35275aaac1a5e857c50c9bbfc182da493f
Anonymous <anonymous@example.com>
2024-07-16 08:29:30 +0530

    added port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 10000;
+//const port = process.env.PORT || 10000;
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
@@ -61,7 +61,7 @@ app.get('/', (req, res) => {
   res.send('Hello Foodi Client Server!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
